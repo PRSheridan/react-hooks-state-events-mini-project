c065ca3b97f841d3b1020ff427a16e24ee87078e
Phil <philrsheridan@gmail.com>
2024-01-26 16:53:58 -0500

    cleaned up TaskList, still not recognizing all tasks listed

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,29 +1,30 @@
 import React from "react";
 import Task from "./Task"
-//restructure taskList..?
 
 function TaskList( {tasks, setTasks, category} ) {
+  function renderTasks() {
+  }
+
   function handleDeleteTask(event) {
     const tempTasks = [...event.target.parentNode.children][1].textContent;
     setTasks(tasks = tasks.filter((task) => tempTasks !== task.text))
   }
 
-//tasks are not rendered at once... because I am filtering them here
-//potentially handle in function?
   return (
     <div className="tasks">
-      {tasks
-      .filter((task) => category === "All" || task.category === category)
-      .map((task) => {
+      {tasks.filter((task) => category === "All" || task.category === category)
+        .map((task, i) => {
+          console.log(task)
           return (
             <Task 
-              key={task.text}
+              key={i}
               category={task.category} 
               text={task.text}
               deleteTask={handleDeleteTask}
             />
           )
-        })}
+        })
+    }
     </div>
   );
 }
